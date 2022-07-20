a3a735a38b16cdf24007446bdd6b7dae509dc524
Akshat Mittal <akshatmittal2506@gmail.com>
2022-07-20 14:47:28 +0530

    initial commit

---
+++ src/team.js
@@ -0,0 +1,294 @@
+const cooridnators = [
+	{
+		img: "team/2_Shubhi.jpg",
+		name: "Shubhi Dua",
+		position: "Coordinator",
+		link: "https://in.linkedin.com/in/shubhi-dua-65a1a9191?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/2_Kavisha.jpg",
+		name: "Kavisha Gupta",
+		position: "Coordinator",
+		link: "https://in.linkedin.com/in/kavisha-gupta-4969531a3?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/2_Vanshika.jpg",
+		name: "Vanshika Pandey",
+		position: "Coordinator",
+		link: "https://in.linkedin.com/in/vanshika-pandey-a590011a9?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/2_Muskan.png",
+		name: "Muskan Agrawal",
+		position: "Coordinator",
+		link: "https://in.linkedin.com/in/muskan-agrawal-425211191?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		name: "Abhigya Shridhar",
+		position: "Coordinator",
+		link: "https://in.linkedin.com/in/abhigya-shridhar-a73a5018b?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/2_Deepak.jpg",
+		name: "Deepak Prajapati",
+		position: "Coordinator",
+		link: "https://in.linkedin.com/in/this-deepraj?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/2_Devang.png",
+		name: "Devang Sharma",
+		position: "Coordinator",
+		link: "https://in.linkedin.com/in/-devang-sharma-?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		name: "Kunal",
+		position: "Coordinator",
+	},
+	{
+		img: "team/2_Kushagra.jpg",
+		name: "Kushagra Johai",
+		position: "Coordinator",
+		link: "https://in.linkedin.com/in/kushagra-johari?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/2_Piyush.jpg",
+		name: "Piyush Agrawal",
+		position: "Coordinator",
+	},
+	{
+		img: "team/2_Angesh.jpg",
+		name: "Kumar Angesh Singh",
+		position: "Coordinator",
+	},
+	{
+		img: "team/2_Praveen.jpg",
+		name: "Praveen Kumar",
+		position: "Coordinator",
+		link: "https://in.linkedin.com/in/pkspyder007?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/2_Rishabh.jpg",
+		name: "Rishab Baid",
+		position: "Coordinator",
+	},
+	{
+		img: "team/2_Shridhar.jpg",
+		name: "Shridhar Chouksey",
+		position: "Coordinator",
+	},
+	{
+		img: "team/2_Umang.jpg",
+		name: "Umang Sharma",
+		position: "Coordinator",
+		link: "https://www.linkedin.com/in/0729?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B1jP7tCaSQVqvJud1msuTWw%3D%3D",
+	},
+	{
+		img: "team/2_Siddharth.jpg",
+		name: "Siddarth Sharma",
+		position: "Coordinator",
+		link: "https://in.linkedin.com/in/siddharth-sharma-6a2a4b18b?trk=public_post-embed_share-update_update-text",
+	},
+];
+const executives = [
+	{
+		img: "team/1_Mitali.jpg",
+		name: "Mitali Jain",
+		position: "Executive Member",
+		link: "https://in.linkedin.com/in/mitali-jain-6b4b1a204?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/1_Nikita.jpg",
+		name: "Nikita Jain",
+		position: "Executive Member",
+	},
+	{
+		img: "team/1_ShubhiArora.jpg",
+		name: "Shubhi Arora",
+		position: "Executive Member",
+		link: "https://www.linkedin.com/in/shubhi-arora-15a549205?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BGwsbl7zkSXuvZPjlPx0%2F%2FQ%3D%3D",
+	},
+	{
+		img: "team/1_Nimish.jpg",
+		name: "Nimish Tiwari",
+		position: "Executive Member",
+	},
+	{
+		img: "team/1_Mohit Bagdi(2).jpg",
+		name: "Mohit Bagdi",
+		position: "Executive Member",
+	},
+	{
+		img: "team/1_Aman.jpg",
+		name: "Aman",
+		position: "Executive Member",
+	},
+	{
+		img: "team/1_AnshulKumar.jpg",
+		name: "Anshul Kumar",
+		position: "Executive Member",
+		link: "https://in.linkedin.com/in/anshulkumar2311?trk=public_post-embed_share-update_update-text",
+	},
+
+	{
+		img: "team/1_Shubham.jpg",
+		name: "Shubham Agrawal",
+		position: "Executive Member",
+		link: "https://in.linkedin.com/in/shubham-agarwal-64b03b201?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/1_Akshat_Mittal.JPG",
+		name: "Akshat Mittal",
+		position: "Executive Member",
+		link: "https://linkedin.com/in/akshatmittal61",
+	},
+	{
+		img: "team/1_Rohit_kumar_gupta.jpg",
+		name: "Rohit Kumar Gupta",
+		position: "Executive Member",
+	},
+	{
+		img: "team/1_Sahil.jpg",
+		name: "Sahil Sagar",
+		position: "Executive Member",
+		link: "https://in.linkedin.com/in/sahil-sagar-1b1942201?trk=public_post-embed_share-update_update-text",
+	},
+
+	{
+		img: "team/1_VanshSingh.jpg",
+		name: "Vansh Singh",
+		position: "Executive Member",
+		link: "https://www.linkedin.com/in/vansh-singh-0414?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Be%2FeloXOCQ3GQiXLOaP4M%2Fg%3D%3D",
+	},
+	{
+		img: "team/1_akshatkhosya.jpg",
+		name: "Akshat Khosya",
+		position: "Executive Member",
+		link: "https://in.linkedin.com/in/akshat-khosya-7b9321201?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/1_Varun.jpg",
+		name: "Varun Prohit",
+		position: "Executive Member",
+		link: "https://in.linkedin.com/in/varun-prohit-a33939201?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/1_Priyankjeet.jpg",
+		name: "Priyank",
+		position: "Executive Member",
+	},
+	{
+		img: "team/1_Divyansh Jindal.jpg",
+		name: "Divyansh Jindal",
+		position: "Executive Member",
+	},
+	{
+		img: "team/1_Pratham.jpg",
+		name: "Pratham Mirdoddiwar",
+		position: "Executive Member",
+		link: "https://in.linkedin.com/in/pratham-mirdoddiwar-27a570201?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/1_pankaj-singh-rana.png",
+		name: "Pankaj Singh Rana",
+		position: "Executive Member",
+		link: "https://in.linkedin.com/in/nmastepankaj?trk=public_post-embed_share-update_update-text",
+	},
+
+	{
+		img: "team/1_Sayak.jpg",
+		name: "Sayak Mondal",
+		position: "Executive Member",
+		link: "https://in.linkedin.com/in/sayak-mondal-b70384200?trk=public_post-embed_share-update_update-text",
+	},
+	{
+		img: "team/1_Rishi.jpg",
+		name: "Rishi Srivastava",
+		position: "Executive Member",
+	},
+	{
+		img: "team/1_Deepak.jpg",
+		name: "Deepak Gupta",
+		position: "Executive Member",
+	},
+];
+const mentors = [
+	{
+		img: "team/3_Shweta.jpg",
+		name: "Shweta Gurnani",
+		position: "Mentors",
+		link: "https://www.linkedin.com/in/shwetagurnani28?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BhPfb5L5nQW%2B3uq5UhAbrBw%3D%3D",
+	},
+	{
+		img: "team/3_Shreya.jpg",
+		name: "Shreya Agrawal",
+		position: "Mentors",
+	},
+	{
+		img: "team/3_Prashasti.jpg",
+		name: "Prashati Gupta",
+		position: "Mentors",
+	},
+	{
+		img: "team/3_Chandan.jpg",
+		name: "Chandan Sitlani",
+		position: "Mentors",
+	},
+	{
+		img: "team/3_Uttam.jpg",
+		name: "Uttam Kumar",
+		position: "Mentors",
+	},
+	{
+		name: "Surendra Kumar",
+		position: "Mentors",
+	},
+	{
+		img: "team/3_Siddhant.jpg",
+		name: "Siddanth Gandhi",
+		position: "Mentors",
+	},
+	{
+		name: "Chiraag Mittal",
+		position: "Mentors",
+	},
+	{
+		name: "Abdul Qadir",
+		position: "Mentors",
+	},
+	{
+		img: "team/3_varun.jpg",
+		name: "Varun Aditya",
+		position: "Mentors",
+	},
+];
+const office = [
+	{
+		img: "team/3_Yash.jpg",
+		name: "Yash Raj",
+		position: "President",
+	},
+	{
+		img: "team/3_Raghav.jpg",
+		name: "Raghav Bansal",
+		position: "Vice President",
+	},
+];
+const faculty = [
+	{
+		img: "team/director.jpg",
+		name: "Dr. S Selvakumar",
+		position: "Director IIIT Una",
+	},
+	{
+		img: "team/vikram.jpg",
+		name: "Dr. Vikram Kumar",
+		position: "Faculty Coordinator",
+	},
+	{
+		img: "team/ishitavaidya.jpg",
+		name: "Ms. Ishita Vaidya",
+		position: "Faculty Coordinator",
+	},
+];
+
+export { cooridnators, executives, mentors, office, faculty };
