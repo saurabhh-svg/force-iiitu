24bf522fcd30fe318ab7b9451129bce47ebc9eb6
Akshat Mittal <akshatmittal2506@gmail.com>
2022-07-20 14:57:03 +0530

    initiated react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./style.css";
+import Home from "./pages/Home/Home";
 
 const App = () => {
 	AOS.init();
-	return <>App</>;
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</>
+	);
 };
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <main>Home</main>
+  )
+}
+
+export default Home
