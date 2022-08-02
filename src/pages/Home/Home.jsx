f1a071756170db4ac50e6d1b4486dec62c63d1de
Akshat Mittal <akshatmittal2506@gmail.com>
2022-08-02 07:07:44 +0530

    created button component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import man from '../../images/man.svg'
-import './home.css'
+import React from "react";
+import Button from "../../components/Button/Button";
+import man from "../../images/man.svg";
+import "./home.css";
 
 const Home = () => {
-  return (
-    <main className='home'>
-      <section className='home-hero'>
-        <div className='home-hero-image' style={{backgroundImage: `url(${man})`}}></div>
-		<div className='home-hero-content'>
-			<span className='home-hero-content__h1'>FORCE</span>
-			<span className='home-hero-content__h2'>Forum of computer engineers</span>
-		</div>
-      </section>
-    </main>
-  )
-}
+	return (
+		<main className="home">
+			<section className="home-hero">
+				<div
+					className="home-hero-image"
+					style={{ backgroundImage: `url(${man})` }}
+				></div>
+				<div className="home-hero-content">
+					<span className="home-hero-content__h1">FORCE</span>
+					<span className="home-hero-content__h2">
+						Forum of computer engineers
+					</span>
+					<span className="home-hero-content__buttons">
+						<Button
+							text="Know More"
+							color="light-blue"
+							link="/about"
+						/>
+						<Button
+							text="Contact Us"
+							color="light-blue"
+							variant="outline"
+							link="/contact"
+						/>
+					</span>
+				</div>
+			</section>
+		</main>
+	);
+};
 
-export default Home
+export default Home;
