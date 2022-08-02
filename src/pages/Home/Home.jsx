66497c7c5a05d9239fcda21cec37ed727a54bae1
Akshat Mittal <akshatmittal2506@gmail.com>
2022-08-02 06:52:26 +0530

    initiated home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import man from '../../images/man.svg'
 import './home.css'
 
 const Home = () => {
   return (
-    <main>Home</main>
+    <main className='home'>
+      <section className='home-hero'>
+        <div className='home-hero-image' style={{backgroundImage: `url(${man})`}}></div>
+		<div className='home-hero-content'>
+			<span className='home-hero-content__h1'>FORCE</span>
+			<span className='home-hero-content__h2'>Forum of computer engineers</span>
+		</div>
+      </section>
+    </main>
   )
 }
 
