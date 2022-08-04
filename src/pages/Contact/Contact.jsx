1bbaca1f9612c27d8bfb1906f1927a73a97ef676
Akshat Mittal <akshatmittal2506@gmail.com>
2022-08-04 12:22:55 +0530

    completed contact us page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,13 +2,14 @@ import React, { useContext, useEffect, useState } from "react";
 import Button from "../../components/Button/Button";
 import GlobalContext from "../../Context/GlobalContext";
 import socials from "../../utils/socials";
+import contactBanner from "../../images/contact.svg";
 import "./contact.css";
 
 const Contact = () => {
 	const { setHeaderStyle } = useContext(GlobalContext);
 	useEffect(() => {
 		setHeaderStyle({
-			color: "var(--black)",
+			color: "var(--white)",
 		});
 	}, [setHeaderStyle]);
 
@@ -32,57 +33,62 @@ const Contact = () => {
 	};
 	return (
 		<main className="contact">
-			<section className="contact-mail">
-				<h1>Send Us A Message</h1>
-				<form onSubmit={handleSubmit}>
-					<input
-						type="text"
-						placeholder="Type your full name"
-						required
-						autoFocus
-						name="name"
-						value={user.name}
-						onChange={handleChange}
-					/>
-					<input
-						type="email"
-						placeholder="Email Address"
-						required
-						name="email"
-						value={user.email}
-						onChange={handleChange}
-					/>
-					<textarea
-						type="text"
-						placeholder="Type your message here"
-						required
-						rows={5}
-						name="message"
-						value={user.message}
-						onChange={handleChange}
-					></textarea>
-					<Button
-						type="submit"
-						text="Send Message"
-						variant="outline"
-						color="black"
-					/>
-				</form>
-			</section>
-			<section className="contact-socials">
-				<h1>Get In Touch</h1>
-				<div className="contact-socials-container">
-					{socials.map((social, id) => (
-						<a
-							href={social.link}
-							title={social.text + "/" + social.username}
-							className="contact-social"
-							key={id}
-						>
-							<div>{social.icon}</div>
-							<span>{social.username}</span>
-						</a>
-					))}
+			<section className="contact-container" data-aos="zoom-in">
+				<div className="contact-left">
+					<h1>Get In Touch</h1>
+					<form className="contact-form" onSubmit={handleSubmit}>
+						<input
+							type="text"
+							placeholder="Type your full name"
+							required
+							autoFocus
+							name="name"
+							value={user.name}
+							onChange={handleChange}
+						/>
+						<input
+							type="email"
+							placeholder="Email Address"
+							required
+							name="email"
+							value={user.email}
+							onChange={handleChange}
+						/>
+						<textarea
+							type="text"
+							placeholder="Type your message here"
+							required
+							rows={5}
+							name="message"
+							value={user.message}
+							onChange={handleChange}
+						></textarea>
+						<Button
+							type="submit"
+							text="Send Message"
+							variant="outline"
+							color="black"
+						/>
+					</form>
+				</div>
+				<div className="contact-right">
+					<div
+						className="contact-right-top"
+						style={{ backgroundImage: `url(${contactBanner})` }}
+					></div>
+					<div className="contact-right-bottom">
+						{socials.map((social, id) => (
+							<a
+								href={social.link}
+								title={social.text + "/" + social.username}
+								className="contact-social"
+								key={id}
+							>
+								<div>{social.icon}</div>
+								<span>{social.username}</span>
+							</a>
+						))}
+					</div>
 				</div>
 			</section>
 		</main>
