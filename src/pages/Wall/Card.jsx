b240681b83be77e9955f7a3bba60b267c5817d38
Akshat Mittal <akshatmittal2506@gmail.com>
2022-08-23 19:47:09 +0530

    created progothon card

--- src/pages/Wall/Card.jsx
+++ src/pages/Wall/Card.jsx
@@ -1,30 +1,46 @@
-import React from 'react'
-import Button from '../../components/Button/Button'
-import './wall.css'
+import React from "react";
+import Button from "../../components/Button/Button";
+import "./wall.css";
 
-const Card = ({image,background,color,title,subtitle,description,link}) => {
-  return (
-    <div className='event-card' style={{
-        backgroundImage: `url(${background})`,
-        backgroundColor: color
-    }}>
-        <div className='event-card-image'>
-            <img src={image} alt={title} />
-        </div>
-        <div className='event-card-content'>
-            <div className='event-card-content-head'>
-                <span>{title}</span>
-                <span>{subtitle}</span>
-            </div>
-            <div className='event-card-content-body'>
-                {description}
-            </div>
-            <div className='event-card-content-button'>
-                <Button href={link} text='Know More' color='white' />
-            </div>
-        </div>
-    </div>
-  )
-}
+const Card = ({
+	image,
+	background,
+	color,
+	title,
+	subtitle,
+	description,
+	link,
+	href,
+	btnText = "Know More",
+}) => {
+	return (
+		<div
+			className="event-card"
+			style={{
+				backgroundImage: `url(${background})`,
+				backgroundColor: color,
+			}}
+		>
+			<div className="event-card-image">
+				<img src={image} alt={title} />
+			</div>
+			<div className="event-card-content">
+				<div className="event-card-content-head">
+					<span>{title}</span>
+					<span>{subtitle}</span>
+				</div>
+				<div className="event-card-content-body">{description}</div>
+				<div className="event-card-content-button">
+					{href && (
+						<Button href={href} text={btnText} color="white" />
+					)}
+					{link && (
+						<Button link={link} text={btnText} color="white" />
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Card
+export default Card;
--- src/utils/events.js
+++ src/utils/events.js
@@ -1,27 +1,39 @@
-import meraki from '../images/meraki.png'
-import merakiBG from '../images/meraki.jpg'
-import skillabhyudaya from '../images/skillabhyudaya.png'
-import skillabhyudayaBG from '../images/skillabhyudaya.jpg'
+import meraki from "../images/meraki.png";
+import merakiBG from "../images/meraki.jpg";
+import skillabhyudaya from "../images/skillabhyudaya.png";
+import skillabhyudayaBG from "../images/skillabhyudaya.jpg";
+import progothon from "../images/progothon.png";
+import progothonBG from "../images/progothon.jpg";
 
-const events=[
-    {
-        title: 'Meraki',
-        subtitle: 'TechFest of IIIT Una',
-        description: 'A fest of Creativity, Love and the Soul',
-        link: 'https://iiitu-meraki.github.io/',
-        image: meraki,
-        background: merakiBG,
-        color: 'transparent',
-    },
-    {
-        title: 'Skillabhyudaya',
-        subtitle: 'Web Development and Design event of IIIT Una',
-        description: 'A 17 day event for garnishing the skills and teamwork',
-        link: 'https://www.linkedin.com/posts/force-iiitu_iiitiansnetwork-codingevent-skillabhyudaya-activity-6899647087986798592-_Bin',
-        image: skillabhyudaya,
-        background: skillabhyudayaBG,
-        color: 'var(--indigo)',
-    },
-]
+const events = [
+	{
+		title: "Prog-o-thon",
+		subtitle: "Explore Open Source",
+		description: "For the students, by the students",
+		link: "/event/Prog-o-thon",
+		image: progothon,
+		background: progothonBG,
+		color: "var(--indigo)",
+	},
+	{
+		title: "Skillabhyudaya",
+		subtitle: "Web Development and Design event of IIIT Una",
+		description: "A 17 day event for garnishing the skills and teamwork",
+		href: "https://www.linkedin.com/posts/force-iiitu_iiitiansnetwork-codingevent-skillabhyudaya-activity-6899647087986798592-_Bin",
+		image: skillabhyudaya,
+		background: skillabhyudayaBG,
+		color: "var(--indigo)",
+	},
+	{
+		title: "Meraki",
+		subtitle: "TechFest of IIIT Una",
+		description: "A fest of Creativity, Love and the Soul",
+		href: "https://iiitu-meraki.github.io/",
+		image: meraki,
+		background: merakiBG,
+		color: "transparent",
+		btnText: "Visit Website",
+	},
+];
 
-export default events
+export default events;
