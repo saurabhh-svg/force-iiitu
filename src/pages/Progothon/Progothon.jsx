4dde27e4ef58d3d1c12ddf1ff0d5ad05718a3989
Akshat Mittal <akshatmittal2506@gmail.com>
2022-08-24 14:26:27 +0530

    created curtain effect for progothon page

--- src/pages/Progothon/Progothon.jsx
+++ src/pages/Progothon/Progothon.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MaterialIcons from "../../components/MaterialIcons";
 import GlobalContext from "../../Context/GlobalContext";
 import "./progothon.css";
@@ -6,11 +6,76 @@ import progothonBG from "../../images/progothon.jpg";
 
 const Progothon = () => {
 	const { setHeaderStyle } = useContext(GlobalContext);
+	const vh = window.innerHeight / 100;
+	const [asideBar, setAsideBar] = useState({
+		style: {
+			position: "unset",
+			top: 0,
+			left: 0,
+			backgroundColor: "var(--pink)",
+			color: "var(--white)",
+		},
+		index: "01",
+		title: "About",
+	});
 	useEffect(() => {
 		setHeaderStyle({
 			color: "var(--white)",
 		});
-	}, [setHeaderStyle]);
+		window.addEventListener("scroll", () => {
+			if (window.scrollY >= 100 * vh && window.scrollY < 150 * vh) {
+				setAsideBar((p) => ({
+					...p,
+					index: "01",
+					title: "About",
+					style: {
+						...p.style,
+						position: "fixed",
+						backgroundColor: "var(--pink)",
+						color: "var(--white)",
+					},
+				}));
+			} else if (
+				window.scrollY >= 150 * vh &&
+				window.scrollY < 250 * vh
+			) {
+				setAsideBar((p) => ({
+					...p,
+					index: "02",
+					title: "Schedule",
+					style: {
+						...p.style,
+						position: "fixed",
+						backgroundColor: "var(--green)",
+						color: "var(--white)",
+					},
+				}));
+			} else if (
+				window.scrollY >= 250 * vh &&
+				window.scrollY < 325 * vh
+			) {
+				setAsideBar((p) => ({
+					...p,
+					index: "03",
+					title: "F.A.Q.s",
+					style: {
+						...p.style,
+						position: "fixed",
+						backgroundColor: "var(--yellow)",
+						color: "var(--black)",
+					},
+				}));
+			} else {
+				setAsideBar((p) => ({
+					...p,
+					style: {
+						...p.style,
+						position: "unset",
+					},
+				}));
+			}
+		});
+	}, [setHeaderStyle, vh]);
 
 	return (
 		<main className="progothon">
@@ -34,6 +99,47 @@ const Progothon = () => {
 					</button>
 				</div>
 			</section>
+			<main className="progothon-body">
+				<aside className="progothon-aside" style={asideBar.style}>
+					<span>{asideBar.index}</span>
+					<span>{asideBar.title}</span>
+				</aside>
+				<section className="progothon-body-content">
+					<div className="progothon-body-section">
+						<h1>About Prog-o-thon</h1>
+						<p>
+							Prog-o-thon is an initiative by the Technical Clubs
+							of IIIT Una to allow students to experience the
+							power of open source. It is a 2 month program where
+							the ideas submitted by you will be selected and the
+							participants will be given chance to work on them
+							with awesome mentors
+						</p>
+					</div>
+					<div className="progothon-body-section">
+						<h1>About Prog-o-thon</h1>
+						<p>
+							Prog-o-thon is an initiative by the Technical Clubs
+							of IIIT Una to allow students to experience the
+							power of open source. It is a 2 month program where
+							the ideas submitted by you will be selected and the
+							participants will be given chance to work on them
+							with awesome mentors
+						</p>
+					</div>
+					<div className="progothon-body-section">
+						<h1>About Prog-o-thon</h1>
+						<p>
+							Prog-o-thon is an initiative by the Technical Clubs
+							of IIIT Una to allow students to experience the
+							power of open source. It is a 2 month program where
+							the ideas submitted by you will be selected and the
+							participants will be given chance to work on them
+							with awesome mentors
+						</p>
+					</div>
+				</section>
+			</main>
 		</main>
 	);
 };
